6cd5f9692e9455fed2e2857583fda894bee4a602
Kiril Serchenko <serchenko1991@gmail.com>
2018-12-28 22:31:55 +0200

    Readme Added

---
+++ build/precache-manifest.a938c2e369b48551ca9e016dac268679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f21faa2e0379ac0eccca75f16b4ef42c",
+    "url": "/static/media/soccerPlayer.f21faa2e.gif"
+  },
+  {
+    "revision": "1c77643d9da5c5ae5495efb14e0ca4eb",
+    "url": "/static/media/Stadium.1c77643d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e58be28cf1c8f92e3f69",
+    "url": "/static/js/main.e58be28c.chunk.js"
+  },
+  {
+    "revision": "98b729c07db267491c94",
+    "url": "/static/js/1.98b729c0.chunk.js"
+  },
+  {
+    "revision": "e58be28cf1c8f92e3f69",
+    "url": "/static/css/main.8fa75d2c.chunk.css"
+  },
+  {
+    "revision": "d0fc65c72909a7f2284bec06f80f65b0",
+    "url": "/index.html"
+  }
+];
